b6f21dbca01937a5c508e310bf303707e7ed82dd
Gabriel-Vs20 <gabrielvalengasilva412@gmail.com>
2026-07-23 21:14:03 -0300

    subindo projeto vazio, apenas com pastas de components e interfaces

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+export default function Footer () {
+  return (
+    <div>
+        <footer>
+        <p>
+          <span>React + TS CRUD</span> @2021
+        </p>
+      </footer>
+    </div>
+  );
+}
