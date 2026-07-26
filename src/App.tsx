70af46f48e733723c12faee0025549c8c9dccbbf
Gabriel-Vs20 <gabrielvalengasilva412@gmail.com>
2026-07-26 19:11:27 -0300

    importando Itask, criando a mesma, passando de 'argumento' no Form e no List

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import Header from './components/Header';
 import styles from './App.module.css';
 import Form from './components/Form';
 import List from './components/List';
+import {ITask} from './interfaces/Task'
 
 function App() {
+
+  const [taskList, setTaskList] = React.useState<ITask[]>([])
   return (
     <div>
       <Header/>
       <main className={styles.main}>
-        <Form btnText='Criar tarefa'/>
-        <List />
+        <Form btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList}/>
+        <List taskList={taskList}/>
       </main>
       <Footer/>
     </div>    
